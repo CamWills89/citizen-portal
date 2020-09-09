950f86c123908fcec45c7210d1bba9a764520d08
Cameron <cameron.wills89@outlook.com>
2020-09-09 16:47:28 -0500

    tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,29 +53,29 @@ let representativesApi = function () {
 
 //geolocation map from google api
 
-// var map, infoWindow;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 6,
-//   });
-//   infoWindow = new google.maps.InfoWindow();
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
 
-//   // Try HTML5 geolocation.
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function (position) {
-//       var pos = {
-//         lat: position.coords.latitude,
-//         lng: position.coords.longitude,
-//       };
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
 
-//       infoWindow.setPosition(pos);
-//       infoWindow.setContent("You are here.");
-//       infoWindow.open(map);
-//       map.setCenter(pos);
-//     });
-//   }
-// }
+      infoWindow.setPosition(pos);
+      infoWindow.setContent("You are here.");
+      infoWindow.open(map);
+      map.setCenter(pos);
+    });
+  }
+}
 
 //error handling
 //  else {
