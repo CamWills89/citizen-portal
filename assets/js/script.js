24652822a2caea58fb667a7a83c1538f18757a6f
claire p <cpuckett9@gmail.com>
2020-09-08 21:02:37 -0500

    basic html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//javascript google map api
+//claire's api key google civic AIzaSyBuB1fEZKUXfj7JWkm-7hKc8f6JS99iCPE 
+//cameron's google civic api AIzaSyCaQylnKFXTaeh7o8Vuenj8LKnFkcr6nQE
+
+let VotingSearchEl = document.querySelector("#voting-search");
+
+
+$("#voting-search").on("click", function(){
+    $(".modal").addClass(".is-active")
+})
