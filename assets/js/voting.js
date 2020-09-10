a1cb70311fccc310f414d4ce4d113818a88a041a
Cameron <cameron.wills89@outlook.com>
2020-09-10 15:49:32 -0500

    added search for address functionality

--- assets/js/voting.js
+++ assets/js/voting.js
@@ -1,71 +1,88 @@
 //this JS file is for displaying election date and register to vote
 let electionNameEl = document.querySelector(".election-name");
 let electionDayEl = document.querySelector(".election-day");
+let electionDayContainerEl = document.querySelector(".election-display");
 let voterInfoEl = document.querySelector(".voter-info");
+let addressEl = document.querySelector("#search");
+let addressFormEl = document.querySelector("#address-form");
+var websiteName = document.querySelector(".website-name");
+var websiteAddress = document.querySelector(".website-link");
+// console.log(addressEl);
 
-// console.log(voterInfoEl);
+var searchHandler = function (event) {
+  event.preventDefault();
+  // //get the text from the input field
+  var address = addressEl.value.trim();
+  // //use the text to display weather data and create a search history list
+  if (address) {
+    // console.log(address);
+    addressEl.value = "";
+    electionDayContainerEl.textContent = "";
+    electionDisplay(address)
+  } else {
+    return;
+  }
+}
 
-// let selectElection = function() {
-//   let apiUrl =
-//     "https://civicinfo.googleapis.com/civicinfo/v2/elections?key=AIzaSyCaQylnKFXTaeh7o8Vuenj8LKnFkcr6nQE";
-
-//   fetch(apiUrl)
-//     .then(function (response) {
-//       return response.json();
-//       // console.log(response);
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       var electionEL = document.getElementById('election')
-//       // console.log(election);
-      
-//       for (var i = 1; i < data.elections.length; i++) {
-        
-//         console.log(data.elections[i].id);
-//         console.log();
-//         var electionOption = document.createElement("option");
-//         electionOption.setAttribute("value", data.elections[i].id)
-//         electionOption.textContent = data.elections[i].name
-//         electionEL.appendChild(electionOption)
-//       }
-//     });
-// };
-
-let electionDisplay = function () {
+let electionDisplay = function (address) {
   let apiUrl =
     "https://civicinfo.googleapis.com/civicinfo/v2/elections?key=AIzaSyCaQylnKFXTaeh7o8Vuenj8LKnFkcr6nQE";
 
   fetch(apiUrl)
     .then(function (response) {
-      return response.json();
-      // console.log(response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        document.location.replace("./index.html");
+      }
     })
     .then(function (data) {
-      var electionEL = document.getElementById('election')
-      // console.log(election);
-      
-      for (var i = 1; i < data.elections.length; i++) {
-        
-        console.log(data.elections[i].id);
-        console.log();
-        var electionOption = document.createElement("option");
-        electionOption.setAttribute("value", data.elections[i].id)
-        electionOption.textContent = data.elections[i].name
-        electionEL.appendChild(electionOption)
-      }
-      
-      console.log(data);
 
-      electionNameEl.innerHTML = data.elections[3].name;
-      var electionDay = moment(data.elections[3].electionDay).format(
-        "dddd, MMMM, Do, YYYY");
-      electionDayEl.innerHTML = electionDay;
+      //if we want a dropdown
+      // var electionEL = document.getElementById('election')
+      // console.log(election);
+
+      // for (var i = 1; i < data.elections.length; i++) {
+
+      //   // console.log(data.elections[i].id);
+      //   // console.log();
+      //   var electionOption = document.createElement("option");
+      //   electionOption.setAttribute("value", data.elections[i].id)
+      //   electionOption.textContent = data.elections[i].name
+      //   electionEL.appendChild(electionOption)
+      // }
+
+      // console.log(data);
+
+      //this is to display all upcoming elections
+      for (let i = 1; i < data.elections.length; i++) {
+          var electionName = document.createElement("h2");
+          electionName.textContent = data.elections[i].name;
+          electionDayContainerEl.appendChild(electionName);
+          var electionDate = moment(data.elections[i].electionDay).format(
+            "dddd, MMMM, Do, YYYY");
+            
+            var electionDay = document.createElement("p");
+            electionDay.textContent = electionDate;
+            electionDayContainerEl.appendChild(electionDay);
+        
+      }
+    
+      //if we want to just display general election
+      // electionNameEl.innerHTML = data.elections[3].name;
+      // var electionDay = moment(data.elections[3].electionDay).format(
+      //   "dddd, MMMM, Do, YYYY");
+      // electionDayEl.innerHTML = electionDay;
 
       let voterApiUrl =
-        "https://civicinfo.googleapis.com/civicinfo/v2/voterinfo?address=8553%20N%20Capital%20Of%20Texas%20Hwy%2C%201107&returnAllAvailableData=true&requestBody=true&electionId=7000&key=AIzaSyCaQylnKFXTaeh7o8Vuenj8LKnFkcr6nQE";
+        "https://civicinfo.googleapis.com/civicinfo/v2/voterinfo?address=" + address + "&returnAllAvailableData=true&requestBody=true&electionId=7000&key=AIzaSyCaQylnKFXTaeh7o8Vuenj8LKnFkcr6nQE";
       fetch(voterApiUrl)
         .then(function (response) {
-          return response.json();
+          if (response.ok) {
+            return response.json();
+          } else {
+            document.location.replace("./index.html");
+          }
         })
         .then(function (data) {
           var siteName = data.state[0].electionAdministrationBody.name;
@@ -73,13 +90,10 @@ let electionDisplay = function () {
             data.state[0].electionAdministrationBody.electionInfoUrl;
           //   console.log(data);
 
-          var websiteName = document.createElement("h3");
-          var websiteAddress = document.createElement("a");
-
           websiteName.textContent =
             "Visit the " +
             siteName +
-            " website to learn about more election details";
+            " website to learn more details about upcoming elections relevant to you.";
           websiteAddress.textContent = "Click Here to go to the Website";
           websiteAddress.setAttribute("href", siteAddress);
           websiteAddress.setAttribute("target", "_blank");
@@ -92,5 +106,6 @@ let electionDisplay = function () {
     });
 };
 
-electionDisplay();
+// electionDisplay();
 // selectElection();
+addressFormEl.addEventListener("submit", searchHandler);
