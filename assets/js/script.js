0ae0f355215c97fe35906c1f4718da28d3615d90
claire p <cpuckett9@gmail.com>
2020-09-11 11:37:09 -0500

    local storage bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,7 @@
 //claire's api key google civic AIzaSyBuB1fEZKUXfj7JWkm-7hKc8f6JS99iCPE
 //cameron's google civic api AIzaSyCaQylnKFXTaeh7o8Vuenj8LKnFkcr6nQE
 
-let VotingSearchEl = document.querySelector("#voting-search");
 
-$("#voting-search").on("click", function () {
-    window.location = "voting.html"
-});
 
    // Try HTML5 geolocation.
    if (navigator.geolocation) {
@@ -31,11 +27,13 @@ $("#representative-btn").on("click", function () {
     var userAddress = $("#address").val() 
     console.log(userAddress)
     console.log("click!");
-    //BUG doesn't prompt for address if there's something in local storage and text input is empty, overwrites "address" value as nothing
+    
     if (userAddress==="" & !localStorage.getItem("address")){
         $("#error-modal").addClass("is-active")
-    } else if (userAddress==""& localStorage.getItem("address")) {
+    } else if (localStorage.getItem("address")) {
+        $("#address").val(localStorage.getItem("address"))
         window.location = "candidates.html"
+  
     } else {
         localStorage.setItem("address", userAddress)
         window.location = "candidates.html"
@@ -50,7 +48,8 @@ $("#voting-search").on("click", function () {
     //BUG doesn't prompt for address if there's something in local storage and text input is empty, overwrites "address" value as nothing
     if (userAddress==="" & !localStorage.getItem("address")){
         $("#error-modal").addClass("is-active")
-    } else if (userAddress==""& localStorage.getItem("address")) {
+    } else if (localStorage.getItem("address")) {
+        $("#address").val(localStorage.getItem("address"))
         window.location = "voting.html"
     } else {
         localStorage.setItem("address", userAddress)
@@ -59,3 +58,4 @@ $("#voting-search").on("click", function () {
 });
 
  
+
